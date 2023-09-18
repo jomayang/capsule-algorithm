6a7bfe92216713cc1f08d58c691a82e91f384e1c
richard <hannemanrichard7@gmail.com>
2023-09-18 12:53:50 +0100

    refactoring colorize algorithm functions and finalizing colorize_emergency_sequence

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def retrieve_pathological_seq(path):
 
     paths = sorted(glob.glob(path + "/*.jpg")) 
     
-    len_ = 0
+    sequence_length = 0
     start = 0
     is_moving = False
     
@@ -15,74 +15,64 @@ def retrieve_pathological_seq(path):
     
     for img_path in paths:
     
-      if (image_mode_check(img_path)=="RGB"):
+      if image_mode_check(img_path) == "RGB":
         
         if is_moving == False:
           start = paths.index(img_path)
-          len_ = 1
+          sequence_length = 1
           is_moving = True
         else:
-          len_ = len_ + 1
+          sequence_length = sequence_length + 1
     
       else:
-        if is_moving:
-          pathological_positions.append((start, len_))
-          pathological_pointers.append((start, (start + (len_ - 1)) + 1))
-          len_ = 0
+        if is_moving == True:
+          end = (start + (sequence_length - 1)) + 1
+          pathological_positions.append((start, sequence_length))
+          pathological_pointers.append((start, end))
+          
+          sequence_length = 0
           start = 0
           is_moving = False
         else:
           continue
+    
     print("[Done] retrieve pathological sequences pointers indeces")
 
     return paths, pathological_pointers, pathological_positions
 
-def colorize_emergency_seq(paths, pathological_pointers, len_emergency_seq, model):
+def colorize_emergency_sequences(paths, pathological_pointers, emergency_sequence_length, model):
+  
+  if len(pathological_pointers) == 1:
+    print("One pathological sequence has been detected...")
+  else:
+    print(f"{len(pathological_pointers)} pathological sequences has been detected...")
 
-    # case 1: one pathsological sequence has been found
+  number_of_iterations = maximum(
+       len(paths[:pathological_pointers[0][0]]), 
+       len(paths[pathological_pointers[0][1]:-1])
+       ) if len(pathological_pointers) == 1 else emergency_sequence_length
+  
+  for sequence in pathological_pointers:  
     
-    if len(pathological_pointers)<=1:
-    
-      print("One pathological sequence has been detected...")
-      print("------------ Pathological sequence ------------")
-      #visualize_rgb(paths[pathological_pointers[0][0]:pathological_pointers[0][1]])
-      max_ = maximum(len(paths[:pathological_pointers[0][0]]), len(paths[pathological_pointers[0][1]:-1]))
-    
-      for x in range(max_):
-    
-        if x <= len(paths[:pathological_pointers[0][0]]): # To colorize prior sequence
-          im_i = paths[(pathological_pointers[0][0] - x)]
-          im_i = colorized_rgb(im_i, model)
-          #visualize_colorized_image(im_i)
-    
-        if x <= len(paths[pathological_pointers[0][1]:-1]): # To colorize next sequence
-          im_j = paths[(pathological_pointers[0][1] + x)]
-          im_j = colorized_rgb(im_j, model)
-          #visualize_colorized_image(im_j)
-    
-    # case 2: more than one pathsological sequences to colorize for emergency use
-    else:
-    
-      print(f"{len(pathological_pointers)} pathological sequences has been detected...")
-      
-      for sequence in pathological_pointers:
-        
-        #visualize_rgb(paths[(sequence[0]+1):sequence[1]]) 
-        print(f"Colorizing the {sequence} is on progress..")
-    
-        for x in range(len_emergency_seq):
-    
-          if x <= len_emergency_seq:  
-            
-            im_i = paths[(sequence[0] - x)]
-            im_i = colorized_rgb(im_i, model)
-            #visualize_colorized_image(im_i) 
-    
-            im_j = paths[(sequence[1] + x)]
-            im_j = colorized_rgb(im_j, model)
-            #visualize_colorized_image(im_j)
-    
-    return print("[Done] Colorization of all emergency sequence.")
+    print(f"Colorizing the {sequence} is on progress..")
+
+    for iteration_index in range(number_of_iterations):
+
+      first_pathological_image_position = sequence[0]
+      last_pathological_image_position = sequence[1]
+
+      # Colorize image before pathological sequence with iteration_index
+      if iteration_index <= len(paths[:first_pathological_image_position]): 
+        im_i = paths[first_pathological_image_position - iteration_index]
+        im_i = colorized_rgb(im_i, model)
+
+      # Colorize image after pathological sequence with iteration_index
+      if iteration_index <= len(paths[last_pathological_image_position:-1]): 
+        im_j = paths[last_pathological_image_position + iteration_index]
+        im_j = colorized_rgb(im_j, model)
+
+  print("[Done] Colorization of all emergency sequence.")
+
 
 def colorize_rest_of_img(paths, pathological_pointers, len_emergency_seq, model):
 
